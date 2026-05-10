8b3d5f2fd5a298b93f200e615d90e6b0428cf704
Danell Oosthuizen <danelloosthuizen3@gmail.com>
2026-05-10 14:47:27 +0200

    Fix output directory to dist

--- frontend/seoforge/src/App.tsx
+++ frontend/seoforge/src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
-import { useEffect, type ComponentType } from "react";
+import { useEffect, useState, type ComponentType } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "@/components/ui/toaster";
